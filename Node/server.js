196fa874ae22a8bc0f5e842855fb5dbd093390fa
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-06 13:07:48 -0300

    Node

--- Node/server.js
+++ Node/server.js
@@ -1,4 +1,37 @@
 const express = require('express');
+const mysql = require('mysql');
+
+// Iniciando o App
 const app = express();
+
+// Iniciando o DB
+const con = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: 'alexbarbosa',
+  database: 'rocketseat_starter',
+});
+
+con.connect((err) => {
+  if (err) {
+    console.log('Erro ao conectar no banco de dados!');
+    return;
+  }
+  console.log('Conexão com banco de dados realizada com sucesso!');
+});
+/*
+con.end((err) => {
+  if (err) {
+    console.log('Erro ao encerrar a conexão com o banco de dados');
+    return;
+  }
+  console.log('Conexão com o banco de dados encerrada com sucesso');
+});
+*/
+// Primeira rota
+app.get('/', (req, res) => {
+  res.send('Alex Felipe Barbosa');
+});
 
 app.listen(3001);
